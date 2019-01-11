ca84e2bc4c16d6f8d65f4f86addcef770c3fe292
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-11 15:48:04 -0800

    component App

---
+++ client/component/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Hello from Component!</div>;
+  }
+}
+
+export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./component/App.jsx";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div>Hello from React!</div>;
-  }
-}
+import App from "./component/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("app"));
