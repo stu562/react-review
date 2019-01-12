6020ba36e9ea67f2f17e5ce4ce0fd391f4193703
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-11 16:26:55 -0800

    passing props down

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -3,15 +3,11 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      first: this.props.firstName,
-      last: this.props.lastName
-    };
   }
   render() {
     return (
       <div>
-        Hello from Component! {this.state.first} {this.state.last}
+        Hello from Component! {this.props.firstName} {this.props.lastName}
       </div>
     );
   }
