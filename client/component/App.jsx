f6cef6e4abe2b2c53ed75da1de8e2bcc45ecd3d6
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-11 16:34:38 -0800

    first name input changes state

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -3,15 +3,25 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.enterFirst = this.enterFirst.bind(this);
     this.state = {
       first: this.props.firstName,
       last: this.props.lastName
     };
   }
+
+  enterFirst(e) {
+    console.log(this);
+    this.setState({
+      first: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
-        Hello from Component! {this.state.first} {this.state.last}
+        {this.state.first} {this.state.last} <br />
+        First: <input name="first" onChange={this.enterFirst} />
       </div>
     );
   }
