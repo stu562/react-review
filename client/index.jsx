3c1a11f1cd56be0b54fe5c05b6833916597fd2e3
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-15 20:36:42 -0800

    exercise1

--- client/index.jsx
+++ client/index.jsx
@@ -1,14 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./component/App.jsx";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div>Hello from React!</div>;
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render("hello from react!", document.getElementById("app"));
