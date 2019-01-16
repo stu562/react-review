b8b499470a178d4f38ef3cec517d5c0f246c3a25
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-15 20:07:35 -0800

    exercise9

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App.jsx";
 
-ReactDOM.render(
-  <App firstName="Julie" lastName="Im" />,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
