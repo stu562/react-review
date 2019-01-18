e8e2047f064964a3e52f6cf38275c7fd8d5b3c11
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-17 17:42:28 -0800

    exercise3

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        Hello from Component! {this.props.firstName} {this.props.lastName}
-      </div>
-    );
-  }
-}
+var App = props => (
+  <div>
+    {props.firstName} {props.lastName}
+  </div>
+);
 
 export default App;
